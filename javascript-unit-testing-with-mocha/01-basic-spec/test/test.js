25cb71a47ad22a99afa74d5b4d65ea8bc81893c8
Pradeep Kumar <pradeep.vwa@gmail.com>
2019-09-20 09:28:46 +0530

    01-basic-spec

---
+++ javascript-unit-testing-with-mocha/01-basic-spec/test/test.js
@@ -0,0 +1,10 @@
+var assert = require('assert');
+describe('Basic Mocha String Test', function () {
+ it('should return number of charachters in a string', function () {
+        assert.equal("Hello".length, 5);
+    });
+ it('should return first charachter of the string', function () {
+        assert.equal("Hello".charAt(0), 'H');
+        //throw {myError:'throwing error to fail test'}
+    });
+});
